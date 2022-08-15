fb320ce22da1f331fc477b317113d87bd447b413
ahmadSaeedGoda <ahmad.saeed.goda@gmail.com>
2022-08-15 13:42:00 +0200

    Refactor App Component Path
    - Because it became a `Stateful` Component since the last commit,
    the `App` component needed to be replaced or moved into outside
    the stateless directory.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import AddProduct from "../../AddProduct";
-import { fetchDataService } from '../../../services/fetchDataService';
-import getBaseUrl from '../../../services/serverUrlRetriever';
-import MainContent from "../../MainContent";
-import NoContent from "../NoContent";
-import { productTypesResponseToProductTypeModelsArrayTransformer } from '../../../transformers/productTypesResponseToProductTypeModelsArray.transformer';
-import { productTypesResponseToDropdownOptionsListTransformer } from '../../../transformers/productTypesResponseToDropdownOptionsList.transformer';
+import AddProduct from "../AddProduct";
+import { fetchDataService } from '../../services/fetchDataService';
+import getBaseUrl from '../../services/serverUrlRetriever';
+import MainContent from "../MainContent";
+import NoContent from "../Stateless/NoContent";
+import { productTypesResponseToProductTypeModelsArrayTransformer } from '../../transformers/productTypesResponseToProductTypeModelsArray.transformer';
+import { productTypesResponseToDropdownOptionsListTransformer } from '../../transformers/productTypesResponseToDropdownOptionsList.transformer';
 import './App.css';
 
 export default function App () {
